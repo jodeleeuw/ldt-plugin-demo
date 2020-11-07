b4c57f3dda3bda63a153085e8788325c218d69f0
Josh de Leeuw <josh.deleeuw@gmail.com>
2020-11-07 14:22:30 -0500

    init commit

---
+++ jspsych-audio-ldt.js
@@ -0,0 +1,186 @@
+/**
+ * jspsych-audio-ldt
+ *
+ **/
+
+jsPsych.plugins["audio-ldt"] = (function() {
+
+  var plugin = {};
+
+  jsPsych.pluginAPI.registerPreload('audio-ldt', 'audio', 'audio');
+
+  plugin.info = {
+    name: 'audio-keyboard-response',
+    description: '',
+    parameters: {
+      audio: {
+        type: jsPsych.plugins.parameterType.AUDIO,
+        pretty_name: 'Audio',
+        default: undefined,
+        description: 'The audio to be played.'
+      },
+      choices: {
+        type: jsPsych.plugins.parameterType.KEYCODE,
+        pretty_name: 'Choices',
+        array: true,
+        default: jsPsych.ALL_KEYS,
+        description: 'The keys the subject is allowed to press to respond to the stimulus.'
+      },
+      prompt: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Prompt',
+        default: null,
+        description: 'Any content here will be displayed below the stimulus.'
+      },
+      trial_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'The maximum duration to wait for a response.'
+      },
+      response_ends_trial: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Response ends trial',
+        default: true,
+        description: 'If true, the trial will end when user makes a response.'
+      },
+      trial_ends_after_audio: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Trial ends after audio',
+        default: false,
+        description: 'If true, then the trial will end as soon as the audio file finishes playing.'
+      },
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    var trial_data = {}
+    trial_data.rt = [];
+
+    // setup audio stimulus, including call to end_trial
+    // when the audio file ends.
+    var context = jsPsych.pluginAPI.audioContext();
+    if(context !== null){
+      var source = context.createBufferSource();
+      source.buffer = jsPsych.pluginAPI.getAudioBuffer(trial.stimulus);
+      source.connect(context.destination);
+      source.onended = function() {
+        end_trial();
+      }
+    } else {
+      var audio = jsPsych.pluginAPI.getAudioBuffer(trial.stimulus);
+      audio.currentTime = 0;
+      audio.addEventListener('ended', end_trial);
+    }
+
+    // show fixation
+    display_element.innerHTML = `<p style="font-size: 32px;">+</p>`
+  
+    // start audio
+    if(context !== null){
+      startTime = context.currentTime;
+      source.start(startTime);
+    } else {
+      audio.play();
+    }
+
+    jsPsych.pluginAPI.setTimeout(next_ldt, trial.fixation_duration)
+
+    function next_ldt() {
+      var current_trial = trial_data.rt.length;
+      display_element.innerHTML = trial.words[current_trial];
+      jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    }
+
+    // function to handle responses by the subject
+    function after_response(info) {
+
+      // only record the first response
+      if (response.key == null) {
+        response = info;
+      }
+
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
+
+    // function to end trial when it is time
+    function end_trial() {
+
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
+
+      // stop the audio file if it is playing
+      // remove end event listeners if they exist
+      if(context !== null){
+        source.stop();
+        source.onended = function() { }
+      } else {
+        audio.pause();
+        audio.removeEventListener('ended', end_trial);
+      }
+
+      // kill keyboard listeners
+      jsPsych.pluginAPI.cancelAllKeyboardResponses();
+
+      // gather the data to store for the trial
+      if(context !== null && response.rt !== null){
+        response.rt = Math.round(response.rt * 1000);
+      }
+      var trial_data = {
+        "rt": response.rt,
+        "stimulus": trial.stimulus,
+        "key_press": response.key
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    };
+
+    
+
+   
+
+    // start the response listener
+    if(context !== null) {
+      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'audio',
+        persist: false,
+        allow_held_key: false,
+        audio_context: context,
+        audio_context_start_time: startTime
+      });
+    } else {
+      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    }
+
+    // end trial if time limit is set
+    if (trial.trial_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, trial.trial_duration);
+    }
+
+  };
+
+  return plugin;
+})();
